033196b5cdb98cebbbff941e866f9abe15f48a49
Veeno_Dev <77930262+Veenoway@users.noreply.github.com>
2022-10-31 19:07:34 +0100

    Day2 continuation

---
+++ helper.hs
@@ -0,0 +1,3 @@
+-- show = convert to string
+-- unwords = retire space between array of words
+-- abs = Absolute value.
--- types.hs
+++ types.hs
@@ -3,3 +3,18 @@
 -- Integer === Integer without limits 
 -- Float === Number with "," 25.132742
 -- Double === Float with double precision 25.132741228718345
+-- LIsts === Array
+-- Tuples === () instead of [] Every value will have his own type 
+-- ++ === Adding two list together
+-- concat === combine all element of a list
+-- head return the first item ( "Hello") return h 
+-- tail return the string or list without the first value ("Hello") return ello
+-- init return first item of a list
+-- last return last item of a list
+-- == Check for Equality
+-- /= Check for ineguality
+-- > Greater than
+-- < Lower than
+-- >= Greather or egual to
+-- <= Lower or equal to 
+-- :info give the priority, but it's often better to use
