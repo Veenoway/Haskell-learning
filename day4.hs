566d795a22a88328bc08e5cd8469b73c00e862fa
Veeno_Dev <77930262+Veenoway@users.noreply.github.com>
2022-11-05 16:46:25 +0100

    day4

--- day4.hs
+++ day4.hs
@@ -1,2 +1,5 @@
 greeting :: String  -> String;
 greeting name = "Hello , " ++ name ++ " how are you today ?"
+
+removeExclamationMarks :: String -> String
+removeExclamationMarks str = [ c | c <- str, c /= '!'];
