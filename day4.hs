f2c9923b3dc1907c509f6896e427ed46fe5a931d
Veeno_Dev <77930262+Veenoway@users.noreply.github.com>
2022-11-06 19:47:01 +0100

    filter day4

--- day4.hs
+++ day4.hs
@@ -1,5 +1,13 @@
+import Distribution.FieldGrammar (List)
 greeting :: String  -> String;
 greeting name = "Hello , " ++ name ++ " how are you today ?"
 
 removeExclamationMarks :: String -> String
 removeExclamationMarks str = [ c | c <- str, c /= '!']; -- /= == !== 
+
+    -- Or we can use filter method 
+removeExclamation :: String -> String
+removeExclamation = filter (/='!')
+
+getOdd :: [Int] -> [Int]
+getOdd  = filter odd
