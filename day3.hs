753fd48f68778b19c977af03a1f58f3e2bc64663
Veeno_Dev <77930262+Veenoway@users.noreply.github.com>
2022-11-15 19:12:01 +0100

    revision

--- day3.hs
+++ day3.hs
@@ -88,3 +88,9 @@ fac n  = aux n 1
 --             acc = n*acc
 --         }
 --     }
+
+fac n  = aux n 1
+    where 
+        aux n acc = aux 
+            | n <= 1 = acc
+            | otherwise = aux (n-1) (n * acc)
